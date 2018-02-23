b3fc44fc9b14b39b04f19c32b0958841654bb3dc
John Ford <remoford@gmail.com>
2018-02-23 09:54:30 -0600

    Added a loglikelihood function in preperation for switch to using fminsearch instead of MLE directly.
    Usage looks like:
    
    >> loglikelihood(data, @convolv_2invG_noreset, 6, [0.1 0.1 0.1 0.1 0.1 0.1])
    
    ans =
    
     -337.4006
    
    >> loglikelihood(data, @onestagepdf2, 2, [0.5 0.5])
    
    ans =
    
      -1.3209e+03

---
+++ loglikelihood.m
@@ -0,0 +1,29 @@
+function sum = loglikelihood(data, pdf, numParams, params )
+
+numData = length(data);
+
+probabilities = [];
+sum=0;
+switch numParams
+    case 0
+        probabilities = pdf(data);
+    case 1
+        probabilities = pdf(data,params(1));
+    case 2
+        probabilities = pdf(data,params(1), params(2));
+    case 3
+        probabilities = pdf(data,params(1), params(2), params(3));
+    case 4
+        probabilities = pdf(data,params(1), params(2), params(3), params(4));
+    case 5
+        probabilities = pdf(data,params(1), params(2), params(3), params(4), params(5));
+    case 6
+        probabilities = pdf(data,params(1), params(2), params(3), params(4), params(5), params(6));
+    otherwise
+        fprintf("ERROR: unspported number of arguments\n");
+end
+for i=1:numData
+    sum = sum + log(probabilities(i));
+end
+
+end
