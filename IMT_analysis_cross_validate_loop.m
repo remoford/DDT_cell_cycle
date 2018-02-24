8852f9ad15bf63468dbe91aac1f7c86688c11594
John Ford <remoford@gmail.com>
2018-02-23 21:34:47 -0600

    fixed penalize function

--- IMT_analysis_cross_validate_loop.m
+++ IMT_analysis_cross_validate_loop.m
@@ -211,7 +211,7 @@ C3 = sum((datatrain-C1).^3)/(length(datatrain));
             
             fminsearch_options = optimset('Display','iter','PlotFcns',@optimplotfval);
             myll=@(params)loglikelihood(datatrain, f, 5, params);
-            objfun=@(params)penalize(datatrain, myll, 5, params, [realmin  realmax;realmin  realmax;realmin  realmax;realmin  realmax;realmin  realmax])
+            objfun=@(params)penalize(myll, 5, params, [realmin  realmax;realmin  realmax;realmin  realmax;realmin  realmax;0.001  0.999])
             p=fminsearch(objfun,x0,fminsearch_options);
             
             %[p,conf]=mle(datatrain,'pdf',f,'start',x0, 'upperbound', [Inf Inf Inf Inf 0.9],'lowerbound',[0 0 0 0 0],'options',options);
--- penalize.m
+++ penalize.m
@@ -1,13 +1,14 @@
-function sum = penalize(data, objfun, numParams, params, parambounds)
+function sum = penalize(objfun, numParams, params, parambounds)
 
 outofbounds = 0;
 for i=1:numParams
-    if params(i) < parambounds(i,1) && params(i) > parambounds(i,2)
+    if params(i) < parambounds(i,1) || params(i) > parambounds(i,2)
         outofbounds=1;
     end
 end
 
 if outofbounds == 1
+    fprintf("out of bounds, penalizing!\n");
     sum = 0;
 else
     sum = objfun(params);  
