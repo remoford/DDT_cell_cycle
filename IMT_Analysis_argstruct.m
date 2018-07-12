d6287c13a84433ae2df9978049e58ca6c5d1a18d
rleander <rleander@10.82.76.226>
2018-07-12 10:38:42 -0500

    This code generates argument structures for passing to IMT_Analysis_April2017.

---
+++ IMT_Analysis_argstruct.m
@@ -0,0 +1,18 @@
+%this code can be used to generate argument structures for passing to
+%IMT_Analysis_April2017.  Choose one data set and one model type.
+
+argstruct.data='dmso';
+%argstruct.data='erlot';
+%argstruct.data='chx';
+%argstruct.data='fucci';
+%argstruct.data='pc(';
+%argstruct.data='mcf';
+%argstruct.data='at1';
+
+%argstruct.model='one';
+%argstruct.model='two';
+%argstruct.model='three';
+%argstruct.model='emg';
+%argstruct.model='one_lag';
+%argstruct.model='two_lag';
+%argstruct.model='no_reset';
